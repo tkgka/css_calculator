dbd0fe485a5dc2d3eb341abe04e2a077ff5fcfc5
YSB_uncyclocity <41094016+uncyclocity@users.noreply.github.com>
2020-10-23 14:32:39 +0900

    난독화

--- calculator.js
+++ calculator.js
@@ -1,4 +1,52 @@
-function enter(){
-    // if(window.event.keyCode == 13)
-    //     cal();
+function input_num(val){
+    var input = document.getElementById('input_area').value;
+    if(input=="0")
+        document.getElementById('input_area').value = val;
+    else
+        document.getElementById('input_area').value = input + val;
+}
+
+function input_opr(val){
+    var input = document.getElementById('input_area').value;
+    var end = input.length-1;
+
+    if(isNaN(input[end])){
+        input = input.substr(0,end);
+        document.getElementById('input_area').value = input + val;
+    }
+    else
+        document.getElementById('input_area').value = input + val;
+}
+
+function dot(){
+    var input = document.getElementById('input_area').value;
+    var end = input.length-1;
+
+    if(!isNaN(input[end]))
+        document.getElementById('input_area').value = input + ".";
+    else
+        document.getElementById('input_area').value = input + "0.";
+}
+
+function backspace(){
+    var input = document.getElementById('input_area').value;
+    var end = input.length-1;
+
+    if(input!=0){
+        var backspaced = input.substr(0,end);
+        if(backspaced != "")
+            document.getElementById('input_area').value = backspaced;
+        else
+            document.getElementById('input_area').value = "0";
+    }
+}
+
+function c_key(){ 
+    document.getElementById('input_area').value = "0";
+    document.getElementById('output_area').value = "0";
+}
+
+function answer(){
+    var input = document.getElementById('input_area').value;
+    document.getElementById('output_area').value = eval(input);
 }
