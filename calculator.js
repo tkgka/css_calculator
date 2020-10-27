671808482c9f8a0809287cbbdcbd137c2d22ac31
YSB_uncyclocity <41094016+uncyclocity@users.noreply.github.com>
2020-10-27 10:28:12 +0900

    bug fix

--- calculator.js
+++ calculator.js
@@ -28,6 +28,7 @@ function dot(){
 
     if(!isNaN(input[end]))
         document.getElementById('input_area').value = input + ".";
+    else if(input[end] == "."){}
     else
         document.getElementById('input_area').value = input + "0.";
 }
@@ -36,7 +37,7 @@ function backspace(){
     var input = document.getElementById('input_area').value;
     var end = input.length-1;
 
-    if(input!=0){
+    if(input!="0"){
         var backspaced = input.substr(0,end);
         if(backspaced != "")
             document.getElementById('input_area').value = backspaced;
